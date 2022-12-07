a62486fec8de2ad273b16fa518eeda88f770512d
tmdgh1592 <tmdgh1592@naver.com>
2022-12-07 21:33:29 +0900

    refactor: 불필요한 메서드 제거

--- src/main/kotlin/racingcar/presentation/OutputView.kt
+++ src/main/kotlin/racingcar/presentation/OutputView.kt
@@ -17,10 +17,6 @@ object OutputView {
 
     fun printMessage(message: String) = println(message)
 
-    fun printError(throwable: Throwable) {
-        println(throwable.message)
-    }
-
     fun printError(error: Exception) {
         println(error.message)
     }
