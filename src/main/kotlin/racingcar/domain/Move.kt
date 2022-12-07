16a1a7d5e77ff529b480916e2881845fcdec410a
tmdgh1592 <tmdgh1592@naver.com>
2022-12-07 21:39:40 +0900

    RandomResult 클래스명 변경 (RandomResult -> Move)

--- src/main/kotlin/racingcar/domain/Move.kt
+++ src/main/kotlin/racingcar/domain/Move.kt
@@ -2,12 +2,12 @@ package racingcar.domain
 
 import racingcar.util.FORWARD_CONDITION
 
-enum class RandomResult {
+enum class Move {
     MOVING_FORWARD,
     STOP;
 
     companion object {
-        fun convertDiceResult(number: Int): RandomResult {
+        fun convertMove(number: Int): Move {
             if (number >= FORWARD_CONDITION) {
                 return MOVING_FORWARD
             }
